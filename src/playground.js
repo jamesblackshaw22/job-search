473c2e480cf05967893c1aea5fa17a88b4245ae1
James Blackshaw <james@blackshaw22.com>
2023-02-17 20:46:43 +0000

    Added main page information and details.

--- src/playground.js
+++ src/playground.js
@@ -1,1 +1,7 @@
+const interval = setInterval(() => {
+  console.log("I will print every 2 seconds after the program starts.");
+}, 2000);
 
+setTimeout(() => {
+  clearInterval(interval);
+}, 10000);
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -3,14 +3,23 @@ import userEvent from "@testing-library/user-event";
 
 import MainNav from "@/components/MainNav.vue";
 describe("MainNav", () => {
+  const renderMainNav = () => {
+    render(MainNav, {
+      global: {
+        stubs: {
+          FontAwesomeIcon: true,
+        },
+      },
+    });
+  };
   it("Displays the company name", () => {
-    render(MainNav);
+    renderMainNav();
     const companyName = screen.getByText("JB Jobs");
     expect(companyName).toBeInTheDocument();
   });
 
   it("Displays menu items for navigation", () => {
-    render(MainNav);
+    renderMainNav();
     const navigationMenuItems = screen.getAllByRole("listitem");
     const navigationMenuText = navigationMenuItems.map(
       (item) => item.textContent
@@ -27,7 +36,7 @@ describe("MainNav", () => {
 
   describe("when the user logs in", () => {
     it("displays the user profile picture", async () => {
-      render(MainNav);
+      renderMainNav();
       let profileImage = screen.queryByRole("img", {
         name: /user profile image/i,
       });
